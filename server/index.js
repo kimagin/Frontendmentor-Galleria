790979c7dd3f0c8ee12bda1ed86990d74508774b
kimagin <info@kimagination.net>
2023-04-22 01:28:04 +0200

    Changeing the welcome message

--- server/index.js
+++ server/index.js
@@ -14,7 +14,10 @@ app.use(express.static('public'));
 
 
 app.get('/',(req,res)=>{
-  res.send('Welcome to the Galleria Server...')
+  res.send(`
+  Welcome to the Galleria Server...
+  Find the information about the assets in "./image.json"
+  `)
 })
 
 
@@ -25,28 +28,6 @@ app.listen(PORT,()=>{
 })
 
 
-
-
-// const images = ['arnolfini-portrait','girl-with-pearl-earring',
-// 'guernica','lady-with-an-ermine','mona-lisa','penitent-magdalene','starry-night'
-// ,'the-basket-of-apples','the-boy-in-the-red-vest','the-great-wave-off-kanagawa','the-night-cafe','the-sleeping-gypsy','the-storm-on-the-sea-of-galilee','the-swing','van-gogh-self-portrait'
-// ]
-// const data = []
-
-// images.forEach(img =>{
-//   data.push({
-//     "name": `${img.replace(/-/g, ' ')}`,
-//     "thumbnail": `/assets/${img}/artist.jpg`,
-//     "gallery": `/assets/${img}/gallery.jpg`,
-//     "heroLarge": `/assets/${img}/hero-large.jpg`,
-//     "heroSmall": `/assets/${img}/hero-small.jpg`,
-//     "artist": `/assets/${img}/artist.jpg`
-//   })
-// })
-
-// const jsonFile = JSON.stringify(data)
-
-// console.log(jsonFile)
 
 
 
